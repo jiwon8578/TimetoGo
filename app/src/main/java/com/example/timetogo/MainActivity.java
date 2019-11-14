8fe6fd2766003ffe34d383749195fcdaefc71134
hyolin97 <35828851+hyolin97@users.noreply.github.com>
2019-11-14 21:53:33 +0900

    push message 특정시간 특정 요일

--- app/src/main/java/com/example/timetogo/MainActivity.java
+++ app/src/main/java/com/example/timetogo/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.timetogo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.os.SystemClock;
 import android.widget.TextView;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 //흐름: 버스 번호를 입력 -> 해당 버스에 대한 노선 id가 나옴 -> 해당 노선에 대한 정류장들에 대한 정류장 정보가 나온다(정류장 id, 정류장 이름, 정류장 번호) -> 정류장 id, 노선 id, 순번을 입력하여 해당 정류장에 도착하는 특정 버스들에 대한 정보를 받아볼 수 있다.
 //busRouteNm이 버스 번호, busRouteId가 노선Id, station이 정류소Id
 
 public class MainActivity extends AppCompatActivity {
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private int count = 0;
+
 
     public ArrayList<String> busRouteList = new ArrayList<String>(); //노선Id들의 리스트
     public ArrayList<String> stationList = new ArrayList<String>(); //정류소Id들의 리스트
@@ -35,7 +48,93 @@ public class MainActivity extends AppCompatActivity {
                 getBusAPI(stationList.get(i), busRouteList.get(0), seqList.get(i));
             }
         }
+        Calendar cal=Calendar.getInstance();
+        String strweek=null;
+        int nWeek=cal.get(Calendar.DAY_OF_WEEK);
+        if(nWeek==1){
+            strweek="일요일";
+        }
+        if(nWeek==2){
+            strweek="월요일";
+        }
+        if(nWeek==3){
+            strweek="화요일";
+        }
+        if(nWeek==4){
+            strweek="수요일";
+        }
+        if(nWeek==5){
+            strweek="목요일";
+            int num=0;
+            if(cal.get(Calendar.HOUR)==9){
+                while(num<4){
+                    NotificationSomethings();
+                    num++;
+                    SystemClock.sleep(10*1000);}
+
+            }
+        }
+        if(nWeek==6){
+            strweek="금요일";
+        }
+        if(nWeek==7){
+            strweek="토요일";
+        }
+
+
+
     }
+    public void NotificationSomethings() {
+
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent notificationIntent = new Intent(this, ResultActivity.class);
+        notificationIntent.putExtra("notificationId", count); //전달할 값
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK) ;
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,  PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground)) //BitMap 이미지 요구
+                .setContentTitle("Time to go!!!").setWhen(System.currentTimeMillis())
+
+                .setContentText("상태바 드래그시 보이는 서브타이틀").setVibrate(new long[]{40,300})
+
+                //출처: https://androphil.tistory.com/368?category=423967 [소림사의 홍반장!]
+                // 더 많은 내용이라서 일부만 보여줘야 하는 경우 아래 주석을 제거하면 setContentText에 있는 문자열 대신 아래 문자열을 보여줌
+                //.setStyle(new NotificationCompat.BigTextStyle().bigText("더 많은 내용을 보여줘야 하는 경우..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent) // 사용자가 노티피케이션을 탭시 ResultActivity로 이동하도록 설정
+                .setAutoCancel(true);
+
+        //OREO API 26 이상에서는 채널 필요
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
+            CharSequence channelName  = "노티페케이션 채널";
+            String description = "오레오 이상을 위한 것임";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName , importance);
+            channel.setDescription(description);
+
+            // 노티피케이션 채널을 시스템에 등록
+            assert notificationManager != null;
+            /*PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK  |
+                    PowerManager.ACQUIRE_CAUSES_WAKEUP |
+                    PowerManager.ON_AFTER_RELEASE, "My:Tag");
+            wakeLock.acquire(10000);*/
+            notificationManager.createNotificationChannel(channel);
+
+        }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
+
+        assert notificationManager != null;
+        notificationManager.notify(1234, builder.build()); // 고유숫자로 노티피케이션 동작시킴
+
+    }
+
+
 
     public void getBusAPI(String stId, String busRouteId, String ord) { //getLowArrInfoByStIdList
         TextView status1 = (TextView)findViewById(R.id.result); //파싱된 결과확인

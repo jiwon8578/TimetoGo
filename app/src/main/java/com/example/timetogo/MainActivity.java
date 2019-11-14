1ff0ea6d7f805cbf99173efc890c9cf0974cbe09
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-14 19:25:46 +0900

    버스도착정보 API

--- app/src/main/java/com/example/timetogo/MainActivity.java
+++ app/src/main/java/com/example/timetogo/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.timetogo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.TextView;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.enableDefaults();
+
+        TextView status1 = (TextView)findViewById(R.id.result); //파싱된 결과확인
+
+        boolean in_rtNm = false, in_arrmsg1 = false, in_arrmsg2 = false;
+
+        String rtNm = null, arrmsg1 = null, arrmsg2 = null;
+
+        try {
+            URL url = new URL("http://ws.bus.go.kr/api/rest/arrive/getLowArrInfoByStId?ServiceKey=4Sc5MPbhBeWyQLVQ1JM9AqGWarV75Qk9hG%2FLuWVNl7%2B4hyHn8nP0mGrsxqDJHcgRdrSYT7sjblUpHpqjDcXdbw%3D%3D&stId=112000001");
+            XmlPullParserFactory parserCreator = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserCreator.newPullParser();
+
+            parser.setInput(url.openStream(), null);
+
+            int parserEvent = parser.getEventType();
+            System.out.println("파싱 시작합니다");
+
+            while(parserEvent != XmlPullParser.END_DOCUMENT) {
+                switch(parserEvent) {
+                    case XmlPullParser.START_TAG:
+                        if(parser.getName().equals("rtNm")) {
+                            in_rtNm = true;
+                        }
+                        if(parser.getName().equals("arrmsg1")) {
+                            in_arrmsg1 = true;
+                        }
+                        if(parser.getName().equals("arrmsg2")) {
+                            in_arrmsg2 = true;
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        if(in_rtNm) {
+                            rtNm = parser.getText();
+                            in_rtNm = false;
+                        }
+                        if(in_arrmsg1) {
+                            arrmsg1 = parser.getText();
+                            in_arrmsg1 = false;
+                        }
+                        if(in_arrmsg2) {
+                            arrmsg2 = parser.getText();
+                            in_arrmsg2 = false;
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if(parser.getName().equals("itemList")) {
+                            status1.setText(status1.getText() + "버스번호: " + rtNm + "첫번째 전: " + arrmsg1 + "두번째 전" + arrmsg2+"\n");
+                        }
+                        break;
+                }
+                parserEvent = parser.next();
+            }
+
+        } catch(Exception e) {
+            status1.setText("에러발생");
+        }
     }
 }

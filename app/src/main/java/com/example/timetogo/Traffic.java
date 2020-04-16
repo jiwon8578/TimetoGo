544dde6cf0c8341cdbfd036355de64982730148c
jiwon_h98@naver.com <jiwon_h98@naver.com>
2020-04-16 17:25:58 +0900

    형식: Traffic.java 주석 추가

--- app/src/main/java/com/example/timetogo/Traffic.java
+++ app/src/main/java/com/example/timetogo/Traffic.java
@@ -1,17 +1,14 @@
 package com.example.timetogo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-import java.net.URL;
 
 public class Traffic extends AppCompatActivity {
-
     public int speed = 0;
     public boolean green = false;
     public boolean yellow = false;
     public boolean red = false;
 
+    //교통상황 카테고리로 나누기 
     public void categorize() {
         speed = Bus.averageSpd;
 

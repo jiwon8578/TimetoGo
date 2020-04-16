f4bf0166f3f006a134e83fc06955c15ef062693e
jiwon_h98@naver.com <jiwon_h98@naver.com>
2020-04-16 15:12:55 +0900

    형식: 불필요한 파일 삭제

--- app/src/main/java/com/example/timetogo/MainActivity.java
+++ app/src/main/java/com/example/timetogo/MainActivity.java
@@ -200,14 +200,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),textId,Toast.LENGTH_SHORT).show();
         }
 
-        //OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(MyWorker.class).build();
-        //WorkManager.getInstance(getApplicationContext()).enqueue(uploadWorkRequest);
-
-        /*Constraints constraints = new Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build();
-        OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class)
-                .setConstraints(constraints)
-                .build();
-        WorkManager.getInstance(getApplicationContext()).enqueue(request);*/
 
         /*Thread worker = new Thread() {
             public void run() {
@@ -666,53 +658,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         worker.start();
-    }
-
-    public void NotificationSomethings(String contentText) {
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent notificationIntent = new Intent(this, ResultActivity.class);
-        notificationIntent.putExtra("notificationId", count); //전달할 값
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK) ;
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,  PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground)) //BitMap 이미지 요구
-                .setContentTitle("Time to go!!!").setWhen(System.currentTimeMillis())
-
-                //.setContentText("상태바 드래그시 보이는 서브타이틀").setVibrate(new long[]{40,300})
-                .setContentText(contentText).setVibrate(new long[]{40,300})
-
-                //출처: https://androphil.tistory.com/368?category=423967 [소림사의 홍반장!]
-                // 더 많은 내용이라서 일부만 보여줘야 하는 경우 아래 주석을 제거하면 setContentText에 있는 문자열 대신 아래 문자열을 보여줌
-                //.setStyle(new NotificationCompat.BigTextStyle().bigText("더 많은 내용을 보여줘야 하는 경우..."))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent) // 사용자가 노티피케이션을 탭시 ResultActivity로 이동하도록 설정
-                .setAutoCancel(true);
-
-        //OREO API 26 이상에서는 채널 필요
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
-            CharSequence channelName  = "노티페케이션 채널";
-            String description = "오레오 이상을 위한 것임";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName , importance);
-            channel.setDescription(description);
-
-            // 노티피케이션 채널을 시스템에 등록
-            assert notificationManager != null;
-            /*PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
-            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK  |
-                    PowerManager.ACQUIRE_CAUSES_WAKEUP |
-                    PowerManager.ON_AFTER_RELEASE, "My:Tag");
-            wakeLock.acquire(10000);*/
-            notificationManager.createNotificationChannel(channel);
-
-        }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
-
-        assert notificationManager != null;
-        notificationManager.notify(1234, builder.build()); // 고유숫자로 노티피케이션 동작시킴
     }
 
     @Override
